64eb8b98763365b90a2d975065edfa4aaa2bfb5d
Tanimoto Megumi <65808877+meg-engineer@users.noreply.github.com>
2021-01-31 18:59:06 +0900

    create-react-app & Firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJV3hwJ7Kfno8DwHJywc7UpJBL5Zn60CU",
+  authDomain: "yatta-81a6f.firebaseapp.com",
+  projectId: "yatta-81a6f",
+  storageBucket: "yatta-81a6f.appspot.com",
+  messagingSenderId: "380532707234",
+  appId: "1:380532707234:web:bff430a151ddf04f426cfb",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const db = firebase.firestore();
