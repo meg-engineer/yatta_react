2f1b73523a8fb8e9663d5aae3ed0317916a2cd6e
Tanimoto Megumi <65808877+meg-engineer@users.noreply.github.com>
2021-03-10 00:09:54 +0900

    Fix Header&Footer UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "antd";
 
 const Footer = () => {
   return (
-    <nav style={{ textAlign: "center" }}>
+    <nav style={{ textAlign: "center", padding: "100px" }}>
       <Link to="/about">
-        <h1>Y A T T A !　とは</h1>
+        <Button type="text">Y A T T A !　とは</Button>
       </Link>
     </nav>
   );
