d06998a31496ef3c1b359e00bf364f2fe629a918
Tanimoto Megumi <65808877+meg-engineer@users.noreply.github.com>
2021-03-10 00:59:15 +0900

    Add GSAP

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Header from "../components/Header";
 import joy from "../components/img/joy.svg";
 import happy from "../components/img/happy.svg";
 import SvgImg from "../components/SvgImg";
 
 const About = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    setAnimation();
+  });
+
+  const setAnimation = () => {
+    gsap.to("#joy", {
+      duration: 2,
+      ease: "bounce",
+      scale: 1.5,
+      delay: 1,
+      scrollTrigger: {
+        trigger: "#joy",
+        start: "top 40%", //開始時のトリガー条件
+        end: "bottom 40%", //終了時のトリガー条件
+        scrub: true,
+        markers: true, // マーカー表示
+      },
+    });
+    gsap.to("#happy", {
+      duration: 2,
+      ease: "bounce",
+      scale: 1.5,
+      delay: 1,
+      scrollTrigger: {
+        trigger: "#happy",
+        start: "top 40%", //開始時のトリガー条件
+        end: "bottom 40%", //終了時のトリガー条件
+        scrub: true,
+        markers: true, // マーカー表示
+      },
+    });
+  };
   return (
     <div>
       <Header />
       <main style={{ textAlign: "center", padding: "100px" }}>
         <p>日々の中であった嬉しいこと</p>
-        <img src={joy} alt="joy" style={{ padding: "50px" }} />
+        <img src={joy} alt="joy" style={{ padding: "200px" }} id="joy" />
         <p>自慢したいこと</p>
-        <img src={happy} alt="happy" style={{ padding: "50px" }} />
+        <img src={happy} alt="happy" style={{ padding: "200px" }} id="happy" />
         <p>
           YATTA!はやった！と思える嬉しかったことをカードにしてTwitterでつぶやくことができるアプリです。
         </p>
